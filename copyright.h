168c925943662783a537605d7902011c94e3b5a5
Josh Lothian <lothian@ornl.gov>
2012-11-06 14:03:17 -0500

    Updating copyright information

--- copyright.h
+++ copyright.h
@@ -1,18 +1,21 @@
 #define COPYRIGHT "\
-  This file is part of SystemConfidence.\n\
-\n\
-  Copyright (C) 2012, UT-Battelle, LLC.\n\
-\n\
-  This product includes software produced by UT-Battelle, LLC under Contract No. \n\
-  DE-AC05-00OR22725 with the Department of Energy. \n\
-\n\
-  This program is free software; you can redistribute it and/or modify\n\
-  it under the terms of the New BSD 3-clause software license (LICENSE). \n\
-  \n\
-  This program is distributed in the hope that it will be useful,\n\
-  but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the \n\
-  LICENSE for more details.\n\
-\n\
-  For more information please contact the SystemConfidence developers at: \n\
-  systemconfidence-info@googlegroups.com\n"
+###################################################################################\n\
+#                                                                                 #\n\
+#  This file is part of SystemConfidence.                                         #\n\
+#                                                                                 #\n\
+#  Copyright (C) 2012, UT-Battelle, LLC.                                          #\n\
+#                                                                                 #\n\
+#  This product includes software produced by UT-Battelle, LLC under Contract No. #\n\
+#  DE-AC05-00OR22725 with the Department of Energy.                               #\n\
+#                                                                                 #\n\
+#  This program is free software; you can redistribute it and/or modify           #\n\
+#  it under the terms of the New BSD 3-clause software license (LICENSE).         #\n\
+#                                                                                 #\n\
+#  This program is distributed in the hope that it will be useful,                #\n\
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of                 #\n\
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                   #\n\
+#  LICENSE for more details.                                                      #\n\
+#                                                                                 #\n\
+#  For more information please contact the SystemConfidence developers at:        #\n\
+#  systemconfidence-info@googlegroups.com                                         #\n\
+###################################################################################\n"
